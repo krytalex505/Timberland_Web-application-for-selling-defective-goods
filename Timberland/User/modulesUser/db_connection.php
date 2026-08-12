2909d05a4b80ac00c3d78b14f0681b70ef81412e
krytalex505 <alexkryt055@gmail.com>
2026-08-12 14:19:37 +0300

    Update db_connection.php

--- Timberland/User/modulesUser/db_connection.php
+++ Timberland/User/modulesUser/db_connection.php
@@ -1,9 +1,9 @@
 <?php
 $connection = new mysqli(
-    'sql307.infinityfree.com',
-    'if0_42508245',
-    'ii4bm2JsCGf5UJ',
-    'if0_42508245_timberland_db'
+    '',
+    '',
+    '',
+    ''
 );
 
 if ($connection->connect_error) {
